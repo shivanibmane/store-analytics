58894d0d9a7a41c568a8b144326836a1cbd44ea6
ShivaniMane <shivanimane43@gmail.com>
2025-05-20 16:04:45 +0530

    refactor: rename the file and add the lable for chart axis

--- src/components/UnavailableEmployee/UnavailableEmployeetrends.tsx
+++ src/components/UnavailableEmployee/UnavailableEmployeetrends.tsx
@@ -1,8 +1,7 @@
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts"
+import { CartesianGrid, Label, Line, LineChart, XAxis, YAxis } from "recharts"
 import {
   Card,
   CardContent,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
@@ -12,36 +11,33 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from "@/components/ui/chart"
-
 import { useEffect, useState } from "react"
 
-
 const chartConfig = {
-  exit: {
-    label: "Exit",
+  duration: {
+    label: "Duration",
     color: "#F92609",
   },
 } satisfies ChartConfig;
 
-interface TrendPoints{
- name:string,
- count:number
+interface TrendPoints {
+  name: string,
+  count: number
 }
-const UnavailableEmployeetrends= () => {
-  const[trends,setTrends]=useState<TrendPoints[]>();
-
-    useEffect(() => {
-        (async () => {
-          try {
-            const res = await fetch("http://127.0.0.1:8000/Emp_unavailabilityTrend");
-            if (!res.ok) throw new Error("network");
-            const jsondata = await res.json();
-            setTrends(jsondata);
-          } catch (e) {
-            console.error("Fetch top-cam failed:", e);
-          }
-        })();
-      }, []);
+const UnavailableEmployeetrends = () => {
+  const [trends, setTrends] = useState<TrendPoints[]>();
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:8000/Emp_unavailabilityTrend");
+        if (!res.ok) throw new Error("network");
+        const jsondata = await res.json();
+        setTrends(jsondata);
+      } catch (e) {
+        console.error("Fetch top-cam failed:", e);
+      }
+    })();
+  }, []);
   return (
     <Card className="flex flex-col w-full border-[#F92609]">
       <CardHeader>
@@ -53,13 +49,24 @@ const UnavailableEmployeetrends= () => {
         <ChartContainer config={chartConfig} className="h-[200px] w-full pr-4">
           <LineChart data={trends}>
             <CartesianGrid vertical={false} />
-            <YAxis tickLine={false} axisLine={false} />
+            <YAxis tickLine={false} axisLine={false} >
+              <Label
+                value="Count"
+                angle={-90}
+                position="insideLeft"
+                style={{ textAnchor: "middle", fontSize: 12 }}
+              /></YAxis>
             <XAxis
               dataKey={"time"}
               tickLine={false}
               axisLine={false}
               tickMargin={8}
-            />
+            ><Label
+                value="Time"
+                position="insideBottom"
+                offset={-4}
+                style={{ textAnchor: "middle", fontSize: 12 }}
+              /></XAxis>
             <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
             <Line
               dataKey={"duration"}
@@ -72,9 +79,6 @@ const UnavailableEmployeetrends= () => {
           </LineChart>
         </ChartContainer>
       </CardContent>
-      <CardFooter className="flex-col gap-2 text-sm">
-        <p className="font-semibold">Total Unavailable Employees</p>
-      </CardFooter>
     </Card>
   )
 }
