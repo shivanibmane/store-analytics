e686691bc6c3a9d6c8fb14c3e5b7b8a8488fac8e
ShivaniMane <shivanimane43@gmail.com>
2025-05-27 00:49:10 +0530

    chore: changet the style of selection input button

--- src/components/CameraManagement/CameraManagement.tsx
+++ src/components/CameraManagement/CameraManagement.tsx
@@ -143,7 +143,7 @@ const CameraManagement = () => {
               <Label htmlFor="modules" style={{ color: '#F92609' }} className="mb-2">Modules</Label>
               <Popover>
                 <PopoverTrigger asChild>
-                  <Button variant="outline" className="w-full justify-start text-left overflow-x-auto whitespace-nowrap no-scrollbar">
+                  <Button variant="outline" className="w-full border justify-start text-left overflow-x-auto whitespace-nowrap no-scrollbar">
                     {formData.modules.length > 0
                       ? formData.modules.join(", ")
                       : "None"}
