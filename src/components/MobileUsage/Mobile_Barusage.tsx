dbcfac7341e51d1bf2439f10f58bd3163616b752
Your Name <rawatrishabh76@gmail.com>
2025-05-28 16:04:32 +0530

    adding Selecton loader in Modile usage

--- src/components/MobileUsage/Mobile_Barusage.tsx
+++ src/components/MobileUsage/Mobile_Barusage.tsx
@@ -4,62 +4,65 @@ import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from "@/components/ui/chart"
-
-import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts"
+} from "@/components/ui/chart";
+import { Bar, BarChart, LabelList, XAxis, YAxis } from "recharts";
+import BarChartSkeletonLoader from "../ChartSkeletonLoaders/BarChartSkeletonLoader";
 
 const chartConfig = {
   duration: {
     label: "Duration",
     color: "#F92609",
   },
-} satisfies ChartConfig
+} satisfies ChartConfig;
 
 const Mobile_Barusage = ({ mobileUsageCameraData, isLoading }: any) => {
   return (
-    <Card className="w-full md:w-full h-[270px] p-2 border-[#F92609]">
+    <Card className="w-full md:w-full h-[310px] p-2 border-[#F92609]">
       <CardHeader>
         <CardTitle className="text-center text-[#F92609] text-sm">
           Mobile Usage Camera
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <ChartContainer config={chartConfig} className="h-[200px] w-full">{!isLoading ?
-          <BarChart
-            width={900}
-            height={150}
-            accessibilityLayer
-            data={mobileUsageCameraData}
-            margin={{ top: 10 }}
-          >
-            <YAxis
-              dataKey={"duration"}
-              tickLine={false}
-              axisLine={false}
-              label={{ value: "Duration", angle: -90, position: "insideLeft", offset: 4 }}
-            />
-            <XAxis
-              dataKey={"camera_name"}
-              tickLine={false}
-              axisLine={false}
-              tickMargin={0}
-              label={{ value: "Camera Name", position: "insideBottom", offset: 1 }}
-            />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-            <Bar dataKey="duration" fill="#F92609" radius={6} barSize={50}>
-              <LabelList
-                position="top"
-                offset={2}
-                className="fill-foreground"
-                fontSize={10}
-
+        <ChartContainer config={chartConfig} className="h-[230px] w-full">
+          {!isLoading ? (
+            <BarChart
+              width={900}
+              height={150}
+              accessibilityLayer
+              data={mobileUsageCameraData}
+              margin={{ top: 10 }}
+            >
+              <YAxis
+                dataKey={"duration"}
+                tickLine={false}
+                axisLine={false}
+                label={{ value: "Duration", angle: -90, position: "insideLeft", offset: 4 }}
               />
-            </Bar>
-          </BarChart> : <div className="flex text-lg items-center justify-center h-full"><p>Loading...</p></div>}
+              <XAxis
+                dataKey={"camera_name"}
+                tickLine={false}
+                axisLine={false}
+                tickMargin={2}
+                label={{ value: "Camera Name", position: "insideBottom", offset: 0 }}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+              <Bar dataKey="duration" fill="#F92609" radius={6} barSize={50}>
+                <LabelList
+                  position="top"
+                  offset={2}
+                  className="fill-foreground"
+                  fontSize={10}
+                />
+              </Bar>
+            </BarChart>
+          ) : (
+            <BarChartSkeletonLoader />
+          )}
         </ChartContainer>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default Mobile_Barusage
+export default Mobile_Barusage;
--- src/components/MobileUsage/Mobile_Lineusage.tsx
+++ src/components/MobileUsage/Mobile_Lineusage.tsx
@@ -1,25 +1,26 @@
-
 import {
   CartesianGrid,
   Line,
   LineChart,
   XAxis,
   YAxis
-} from "recharts"
+} from "recharts";
 
 import {
   Card,
   CardContent,
   CardHeader,
   CardTitle
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 
 import {
   ChartConfig,
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent
-} from "@/components/ui/chart"
+} from "@/components/ui/chart";
+
+import LineChartSkeletonLoader from "../ChartSkeletonLoaders/LineChartSkeletonLoader";
 
 const chartConfig = {
   duration: {
@@ -37,36 +38,40 @@ const Mobile_Lineusage = ({ mobileUasgeTrendData, isLoading }: any) => {
         </CardTitle>
       </CardHeader>
       <CardContent>
-        <ChartContainer config={chartConfig} className="h-[200px] w-full p-2">
-          {!isLoading ? <LineChart data={mobileUasgeTrendData}>
-            <CartesianGrid vertical={false} />
-            <YAxis
-              dataKey={"duration"}
-              tickLine={false}
-              axisLine={false}
-              label={{ value: "Duration", angle: -90, position: "insideLeft", offset: 0 }}
-            />
-            <XAxis
-              dataKey={"time"}
-              tickLine={false}
-              axisLine={false}
-              tickMargin={2}
-              label={{ value: "Hours", position: "insideBottom", offset: -5 }}
-            />
-            <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
-            <Line
-              dataKey={"duration"}
-              type="natural"
-              stroke="#F92609"
-              strokeWidth={2}
-              dot={{ fill: "#F92609" }}
-              activeDot={{ r: 5 }}
-            />
-          </LineChart> : <div className="flex text-lg items-center justify-center h-full"><p>Loading...</p></div>}
+        <ChartContainer config={chartConfig} className="h-[220px] w-full p-2">
+          {!isLoading ? (
+            <LineChart data={mobileUasgeTrendData}>
+              <CartesianGrid vertical={false} />
+              <YAxis
+                dataKey={"duration"}
+                tickLine={false}
+                axisLine={false}
+                label={{ value: "Duration", angle: -90, position: "insideLeft", offset: 0 }}
+              />
+              <XAxis
+                dataKey={"time"}
+                tickLine={false}
+                axisLine={false}
+                tickMargin={2}
+                label={{ value: "Hours", position: "insideBottom", offset: -5 }}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+              <Line
+                dataKey={"duration"}
+                type="natural"
+                stroke="#F92609"
+                strokeWidth={2}
+                dot={{ fill: "#F92609" }}
+                activeDot={{ r: 5 }}
+              />
+            </LineChart>
+          ) : (
+            <LineChartSkeletonLoader />
+          )}
         </ChartContainer>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default Mobile_Lineusage
+export default Mobile_Lineusage;
